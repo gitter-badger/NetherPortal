b4080fdc1f5be3ff92be21cba14e802a5736c5ac
Michael N <seetch5@gmail.com>
2015-12-04 22:08:21 +0300

    git rm
    git rm

--- setting.php
+++ setting.php
@@ -1,14 +1,2 @@
 <?php
-function setting(){
-  function s_global(){
-    $title='title';
-    $description='description';
-  }
-  function s_mysql(){
-    $dbHost='localhost'; #
-    $dbName='base'; #
-    $dbUser='user'; #
-    $dbPass='password'; #
-  }
-}
-?>
+
