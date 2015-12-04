bf05a84b27b9b162e98ad4f700c03e1c8edb7432
Michael N <seetch5@gmail.com>
2015-12-04 22:15:16 +0300

    create connect mysqli

--- setting.php
+++ setting.php
@@ -1,2 +1,5 @@
 <?php
-
+  $mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
+  if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+  }
