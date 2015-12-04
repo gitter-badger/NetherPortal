ede9bb0a88c278999283b25ee75fd7411c6bd8f2
Michael N <seetch5@gmail.com>
2015-12-04 19:34:49 +0300

    add mysql setting.

---
+++ setting.php
@@ -0,0 +1,14 @@
+<?php
+function setting(){
+  function s_global(){
+    $title='title';
+    $description='description';
+  }
+  function s_mysql(){
+    $dbHost='localhost'; #
+    $dbName='base'; #
+    $dbUser='user'; #
+    $dbPass='password'; #
+  }
+}
+?>
