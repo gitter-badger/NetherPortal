633fc5b1f4a1aa498fbbc2688e5692f0bed4650b
Michael N <seetch5@gmail.com>
2015-12-05 13:35:28 +0300

    Update setting.php

--- setting.php
+++ setting.php
@@ -4,26 +4,25 @@
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
   } */
   
-  $lang = isset($_GET["lang"])? $_GET["lang"]: "en"; //ru or en! //ru или en!
+  $lang = isset($_GET["lang"])? $_GET["lang"]: "en"; //ru or en!
   $array = parse_ini_file("lang/".$lang.".ini");
   
-  $title = 'NetherPortal'; //site title //заголовок сайта
-  $description = 'PocketMine-MP Web Interface and more...'; //site description //описание сайта
+  $title = 'NetherPortal'; //site title
+  $description = 'PocketMine-MP Web Interface and more...'; //site description 
   
-  /* Widgets Settings - Настройки Виджетов */
+  /* Widgets Settings */
   
-  $a=0; # change to 1 when add a widget - смените на 1,когда добавите виджет
+  $a=0; # change to 1 when add a widget
   $b=1; 
   
   function _widgets($a, $b){
 	  if($a == $b){
 		  echo '
 		    you code
-		  '; #code you widgets - код вашего виджета
+		  '; #code you widgets 
 	  }else{
 		  echo '
 		  connect one of the widgets in config file
-		  подключите один из виджетов в конфигурационном файле
-		  '; # it does not change! - не изменять!
+		  '; # it does not change!
 	  }
   }
